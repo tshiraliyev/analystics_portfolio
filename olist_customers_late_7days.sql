75d06a66ad855d91ef568da122ca74ac3fe152da
Telman Shiraliyev <superherobrine11@gmail.com>
2025-02-13 18:37:13 +0400

    Delete olist_customers_late_7days.sql

--- olist_customers_late_7days.sql
+++
@@ -1,5 +0,0 @@
-select customer_id,round(order_delivered_customer_date-order_purchase_timestamp,1) as "delivery time" from orders
-where (order_delivered_customer_date-order_purchase_timestamp>=7)
-order by customer_id;
-
---defined the customers whose delivery time is more or equal 7
